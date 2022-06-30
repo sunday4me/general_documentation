dc4ef838803d222914a0f218f2f53c7d57aa279b
Sunday A. Jimoh <31360475+sunday4me@users.noreply.github.com>
2022-06-30 15:26:58 +0100

    Create js_note.js

---
+++ js_note.js
@@ -0,0 +1,30 @@
+// How to declare String primitive
+
+const message = "This is my first message";
+
+
+// Declaring String object
+
+const another = new String('hi');
+
+// Declaring Arrays
+
+let selectedColors = [ 'red', 'blue'];
+
+console.log(selectedColors);
+// so if you want to print out red on the console 
+console.log(selectedColors[0]);
+
+// You can add to line 12 by 
+selectedColors[2] = 'purple';
+
+// so if you want to print out all the color to the console 
+console.log(selectedColors);
+
+// Declaring function
+
+function greet() {
+  console.log('Hello World');
+}
+
+greet();
